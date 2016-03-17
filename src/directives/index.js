01c3846aded7213e4cdab756df419ee2eedeed3d
Gabriel Malet <gabrielmalet@gmail.com>
2016-03-17 12:28:10 +0100

    Add slides

--- src/directives/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * Charge les directives
- * @param app
- */
-module.exports = function(app) {
-  // todo
-};
--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * Charge les services
- * @param app
- */
-module.exports = function(app) {
-  // todo
-};
